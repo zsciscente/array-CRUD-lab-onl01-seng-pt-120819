1ad0bf25105d2e1241a49c07d7f0f92ec647c27b
Zachary Sciscente <zsciscente@gmail.com>
2019-12-14 17:49:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element_from_end_of_array = ["arrays!", "clutter"]
+  remove_element_from_end_of_array = ["arrays!", "arrays!"]
   remove_element_from_end_of_array.pop
 end
 
