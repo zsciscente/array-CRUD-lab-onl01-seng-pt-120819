0951646b1d8dfb3f0e2c930755c7d286815f0949
Zachary Sciscente <zsciscente@gmail.com>
2019-12-14 17:55:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   retrieve_element_from_index = ["Mr","jake","joe","am","steve"]
-  retrieve_element_from_index.index(3)
+  retrieve_element_from_index[3]
 end
 
 def retrieve_first_element_from_array(array)
