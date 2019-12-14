7867ec3d76dc6fc9103e929b7e127eea7a399409
Zachary Sciscente <zsciscente@gmail.com>
2019-12-14 17:49:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array = ["arrays!"]
+  remove_element_from_start_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
