3f4e26bc304cc4a4e7423a48a1846fa358257d2d
Zachary Sciscente <zsciscente@gmail.com>
2019-12-14 17:52:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_element_from_index = [1,2,3,"am",5]
+  retrieve_element_from_index.index(3)
 end
 
 def retrieve_first_element_from_array(array)
