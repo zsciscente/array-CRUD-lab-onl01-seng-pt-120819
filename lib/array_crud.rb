5ac2003bd1aaa104851436e66b4967e284295f98
Zachary Sciscente <zsciscente@gmail.com>
2019-12-14 17:47:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
    add_element_to_start_of_array = [1,2,3]
-  add_element_to_start_of_array.pop ("wow")
+  add_element_to_start_of_array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
